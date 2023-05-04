f3d582bf484611a9dd28a95a65bb955106fd10a5
Fengrui Gan <fengruigan@gmail.com>
2023-05-04 16:56:11 -0700

    first attempt to do authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import AppNavbar from "./components/AppNavbar";
 import RecipePage from "./pages/RecipePage";
+import LandingPage from "./pages/LandingPage";
+import AuthPage from "./pages/AuthPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/recipes",
+    element: <RecipePage />,
+  },
+  {
+    path: "/signin",
+    element: <AuthPage signIn={true} />,
+  },
+  {
+    path: "/signup",
+    element: <AuthPage signIn={false} />,
+  },
+]);
 
 const App = () => {
   return (
     <div className="App">
       <AppNavbar />
-      <RecipePage />
+      <RouterProvider router={router} />
     </div>
   );
 };
--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -1,27 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import useAuth from "../hooks/useAuth";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
-function AppNavbar() {
+const AppNavbar = () => {
+  const { getAuthedUserAttributes, verifySession } = useAuth();
+  const [displayEmail, setDisplayEmail] = useState("");
+  const [isSessionValid, setIsSessionValid] = useState(false);
+
+  useEffect(() => {
+    const getUserEmail = async () => {
+      const valid = await verifySession(true);
+      setIsSessionValid(valid);
+      if (valid) {
+        const attirbutes = await getAuthedUserAttributes();
+        const email = attirbutes.filter((el) => el.Name === "email").map((el) => el.Value)[0];
+        setDisplayEmail(email);
+      }
+    };
+    getUserEmail();
+  }, [verifySession, getAuthedUserAttributes]);
+
   return (
     <Navbar className="AppNavbar" bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Recipe Roulette</Navbar.Brand>
+        <Navbar.Brand href="/">Recipe Roulette</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto me-0">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#action/3.1">
-              <FontAwesomeIcon icon={faUser} /> Sign in/up
-            </Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            {isSessionValid ? (
+              <Nav.Link href="/account">
+                <FontAwesomeIcon icon={faUser} className="me-1" />
+                Logged in as {displayEmail}
+              </Nav.Link>
+            ) : (
+              <Nav.Link href="/signin">
+                <FontAwesomeIcon icon={faUserPlus} className="me-1" />
+                Sign in/up
+              </Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
 export default AppNavbar;
